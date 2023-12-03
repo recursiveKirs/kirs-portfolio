a630226618da2c1683ba12a1915216fa9b7f7645
kirs <kirs.l.imsong@gmail.com>
2023-12-02 21:30:40 -0500

    update home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,16 @@ import "./HomePage.css";
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="home-content">
       <p className="body-text">Hey I'm Kirs!</p>
+      <img src="/kirs-home.jpg" className="home-header-image" />
       <p className="body-text">
         I enjoy photography, videography, music, hiking, sailing, camping,
-        weightlifting, and pretending to be a software engineer. Currrently I am
-        looking for a new role so hit me up if you are in the market for
-        solutions dirven fullstack software engineer.
+        weightlifting, and building software solutions. Currrently in the market
+        for a new role so ping me if you need a results-driven Fullstack
+        Software Engineer with 4+ years of experience designing and implementing
+        solutions using JavaScript, React, Ruby on Rails, C#, Node, Typescript,
+        GraphQL, AWS, and SQL!
       </p>
     </div>
   );
